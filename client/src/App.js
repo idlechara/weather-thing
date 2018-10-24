0bffd8e794550015e97d2251978259e60df48ffd
Erik Regla <erik.regla@gmail.com>
2018-10-24 17:50:22 -0300

    feat(maps): added scss and gmaps support

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+ 
 
 class App extends Component {
+  onMapClicked(mapProps, map, clickEvent) {
+    console.log(mapProps);
+    console.log(map);
+    console.log(clickEvent);
+    // Send to microservice
+    // retrieve from microservice
+    // display
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Map google={this.props.google} zoom={2} onClick={this.onMapClicked} />
     );
   }
 }
-
-export default App;
+export default GoogleApiWrapper({
+  apiKey: ('AIzaSyAF_BYy0S9FfOStIU-JZayxNteppkV7YZU')
+})(App)

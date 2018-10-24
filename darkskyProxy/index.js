43a991c1cc61bbbec0ff0ef5db0be83107915e54
Erik Regla <erik.regla@gmail.com>
2018-10-24 17:58:02 -0300

    feat(darksky): testing express

--- darkskyProxy/index.js
+++ darkskyProxy/index.js
@@ -1,7 +1,17 @@
-var express = require('express')
-var app = express()
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.send('hello world')
+app.get('/', urlencodedParser, (req, res) => {
+  console.log(req.body);
+  res.send("hello world");
+});
+
+const server = app.listen(8081, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log("Example app listening at http://%s:%s", host, port);
 });

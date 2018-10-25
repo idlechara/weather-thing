e8a274e1279bdd9bc525471255db4a654711f0d4
erik.regla@gmail.com <erik.regla@gmail.com>
2018-10-25 01:54:41 -0300

    feat(swal): notifications and format

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,60 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
-import axios from 'axios'
+import axios from "axios";
+import swal from "sweetalert2";
 
 class App extends Component {
-  
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   onMapClicked(mapProps, map, clickEvent) {
     let coordinates = {
       lat: clickEvent.latLng.lat(),
       lng: clickEvent.latLng.lng()
     };
 
-    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinates.lat},${coordinates.lng}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`).then(response => {
-      let address = "";
-      try {
-        address = response.data.results.reverse()[1].formatted_address;
-        coordinates = response.data.results.reverse()[1].geometry.location;
-      }
-      catch (err) {
-        // you have not clicked in a country
-      }
+    axios
+      .get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinates.lat},${coordinates.lng}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`)
+      .then(response => {
+        let address = "";
+        try {
+          address = response.data.results.reverse()[1].formatted_address;
+          coordinates = response.data.results.reverse()[1].geometry.location;
 
-      axios.get(`/API/query?lat=${coordinates.lat}&lng=${coordinates.lng}`).then(response => {
-        console.log(response.data);
+          axios
+            .get(`/API/query?lat=${coordinates.lat}&lng=${coordinates.lng}`)
+            .then(response => {
+              swal({
+                title: address,
+                text: `Actualmente hay ${response.data.toFixed(1)} ºC`,
+                type: "success",
+                confirmButtonText: "Ok"
+              });
+            });
+        } catch (err) {
+          swal({
+            title: "¡No has hecho click en un país!",
+            text: `Esto no va funcionar si es que le haces click a un trozo de tierra o agua que no está bajo el dominio de alguien.`,
+            type: "error",
+            confirmButtonText: "Ok"
+          });
+        }
       });
-    })
   }
 
   render() {
     return (
-      <Map google={this.props.google} zoom={2} onClick={this.onMapClicked} />
+      <Map
+        google={this.props.google}
+        onClick={this.onMapClicked}
+        zoom={2}
+        maxZoom={2}
+        minZoom={2}
+      />
     );
   }
 }
